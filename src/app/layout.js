8b9b17fbfb60333b7c844c8ab4f63308fdc9e785
Connoroni <mitchell2k2@gmail.com>
2025-02-14 14:05:37 +0000

    created sign in and sign up pages using clerk components

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      {" "}
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
