b4c4f29423aac85ffc931348c4c42421017f40f8
Connoroni <mitchell2k2@gmail.com>
2025-02-14 15:55:40 +0000

    fixed form by only stringifying the necessary data

--- src/app/create-profile/page.js
+++ src/app/create-profile/page.js
@@ -1,32 +1,51 @@
-import db from "@/utils/dbConnection.js";
+import { db } from "@/utils/dbConnection.js";
 import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function CreateProfile() {
   const clerkUser = await currentUser();
   // console.log(clerkUser);
-  const clerkJson = JSON.stringify(clerkUser);
-  console.log(clerkJson);
+  const emailJson = JSON.stringify(clerkUser.emailAddresses[0].emailAddress);
+  const idJson = JSON.stringify(clerkUser.id);
+  console.log("JSON data test:", emailJson, idJson);
+  // Big credit to Cameron for helping me fix this, I was getting errors saying the values of id and emailAddresses[0].emailAddress were undefined because I was stringifying the whole object. Cameron's solution was to stringify only the bits I needed since there are data types like boolean (and maybe the emailAddresses array) in the object that don't work when stringified.
 
   async function handleSubmit(formValues) {
     "use server";
     const formData = {
-      clerk_id: clerkJson.id,
+      clerk_id: idJson,
       username: formValues.get("username"),
       first_name: formValues.get("first_name"),
       last_name: formValues.get("last_name"),
-      email: clerkJson.emailAddresses[0],
+      email: emailJson,
       profile_pic: formValues.get("profile_pic"),
       location: formValues.get("location"),
       bio: formValues.get("bio"),
     };
+
+    db.query(
+      `INSERT INTO users (clerk_id, username, first_name, last_name, email, profile_pic, location, bio)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+      [
+        formData.clerk_id,
+        formData.username,
+        formData?.first_name,
+        formData?.last_name,
+        formData.email,
+        formData?.profile_pic,
+        formData?.location,
+        formData?.bio,
+      ]
+    );
+    redirect("/");
   }
 
   return (
     <>
       <h1>Create your profile</h1>
       <p>Enter your information in the form below to create your profile:</p>
-      <p>Fields marked with a &lpar;*&rpar; are required.</p>
-      <form>
+      <p>Fields marked with a (*) are required.</p>
+      <form action={handleSubmit} className="flex flex-col">
         <label htmlFor="username">Username*</label>
         <input type="text" name="username" id="username" required />
         <label htmlFor="first_name">First name</label>
@@ -44,6 +63,7 @@ export default async function CreateProfile() {
         <input type="text" name="location" id="location" />
         <label htmlFor="bio">Bio</label>
         <textarea name="bio" id="bio" placeholder="Type your bio here..." />
+        <button type="submit">Create profile</button>
       </form>
     </>
   );
