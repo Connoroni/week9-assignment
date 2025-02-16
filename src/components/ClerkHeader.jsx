5224e6ed3e1edaf7c3df20c408c1daa71a10cd5f
Connoroni <mitchell2k2@gmail.com>
2025-02-16 22:34:15 +0000

    home page layout and functionality, added links to ClerkHeader component

--- src/components/ClerkHeader.jsx
+++ src/components/ClerkHeader.jsx
@@ -5,10 +5,13 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function ClerkHeader() {
   return (
     <header className="flex justify-end items-center p-4 gap-4 h-16">
+      <Link href="/">Home</Link>
+      <Link href="/posts">Posts</Link>
       <SignedOut>
         <SignInButton />
         <SignUpButton />
